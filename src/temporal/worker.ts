fdf7f9a6fcba0f69745d9e46365974aa39d8fc10
benzae1 <anton.erdmann.benz@uni-weimar.de>
2026-05-07 15:27:55 +0200

    Create Temporal worker entrypoint

--- src/temporal/worker.ts
+++ src/temporal/worker.ts
@@ -1,1 +1,29 @@
-export function startWorker(){ }
+import { NativeConnection, Worker } from "@temporalio/worker";
+import * as approvalActivities from "./activities/approvalActivities";
+import * as notificationActivities from "./activities/notificationActivities";
+import * as orgActivities from "./activities/orgActivities";
+
+async function run() {
+  const connection = await NativeConnection.connect({
+    address: process.env.TEMPORAL_ADDRESS ?? "localhost:7233",
+  });
+
+  const worker = await Worker.create({
+    connection,
+    namespace: process.env.TEMPORAL_NAMESPACE ?? "default",
+    taskQueue: "formflow-approval",
+    workflowsPath: require.resolve("./workflows/approvalWorkflow"),
+    activities: {
+      ...approvalActivities,
+      ...notificationActivities,
+      ...orgActivities,
+    },
+  });
+
+  await worker.run();
+}
+
+run().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
